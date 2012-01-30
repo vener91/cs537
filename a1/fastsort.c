adc0a5623ba6ca694bff4bcfdd66c314ea57ca57
Terrence <vener88@gmail.com>
2012-01-30 16:46:02 -0600

    Improved times slightly

--- a1/fastsort.c
+++ a1/fastsort.c
@@ -13,7 +13,6 @@
 static int cmprect(const void *p1, const void *p2) {	
 	//Naive method
 	int i;
-	
 	for(i = 0; i < NUMRECS; i++){
 		if((*(rec_t* const*)p1)->record[i] >  (*(rec_t* const *)p2)->record[i]){
 			return 1;
@@ -22,19 +21,30 @@ static int cmprect(const void *p1, const void *p2) {
 		}
 	}
 	return 0;
-	
-	//Assuming 64 bit machines, uses long
 	/*
-	p1 = p1 + 4; //Offset the number of bytes
-	p2 = p2 + 4;
-	int offset;
-	while(offset < NUMRECS){
-		
-		if(p1 > p2 + offset ){
+	//Assuming 64 bit machines, uses long
+	rec_t recA = *(*(rec_t* const*)p1);
+	rec_t recB = *(*(rec_t* const*)p2);
+	int64_t recRA = (int64_t)recA.record;
+	int64_t recRB = (int64_t)recB.record;
+	int i;
+	int length = NUMRECS / 2; //since size of int64_t is 2x the size of int
+	for(i = 0; i < length; i++){	
+		if(recRA > recRB ){
 			return 1;
+		}else if(recRB < recRA){
+			return -1;
 		}
-		offset = offset + 4;
+		recRA = (int64_t)(&recRA + sizeof(int64_t));
+		recRB = (int64_t)(&recRB + sizeof(int64_t));
 	}
+	//Compare last 4 bytes
+	if((int64_t)(&recRA - sizeof(int)) > (int64_t)(&recRB - sizeof(int)) ){
+		return 1;
+	}else if(recRB < recRA){
+		return -1;
+	}
+
 	return 0;
 	*/
 }
@@ -78,31 +88,29 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr, "Error: Cannot open file %s\n", inFile);
 		exit(1);
 	}
-
 	// Put text in memory
 	int sizeOfArray = fileStat.st_size / sizeof(rec_t);
-	rec_t *block[sizeOfArray];
-	int i = 0;	
-	int rc; //General result int
-	while (1) {	
-		rec_t *r;
-		r = malloc(sizeof(rec_t));
-		rc = read(fd, r, sizeof(rec_t));
-		if (rc == 0) // 0 indicates EOF
-			break;
-		if (rc < 0) {
-			fprintf(stderr, "Error: Cannot open file %s\n", inFile);
-			exit(1);
-		}
-		block[i] = r;
-		i++;
+	rec_t *Rblock;
+	Rblock = malloc(fileStat.st_size);
+	int rc;
+	rc = read(fd, Rblock, fileStat.st_size);
+	if(rc < 0){
+		fprintf(stderr, "Error: Cannot open file %s\n", inFile);
+		exit(1);
 	}
-
 	rc = close(fd);
 	if(rc < 0){
 		fprintf(stderr, "Error: Cannot open file %s\n", inFile);
 		exit(1);
 	}
+	
+	rec_t *block[sizeOfArray];
+	int i = 0;	
+	for(i = 0; i < sizeOfArray; i++){
+		block[i] = &Rblock[i];
+	}
+
+	
 
 	//Sort
 	qsort(block, sizeOfArray, sizeof(rec_t *), cmprect);	
