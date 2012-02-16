23258c225f7fffb08e261c89f16432d695a7b574
Terrence <vener88@gmail.com>
2012-02-16 16:29:22 -0600

    Checkpoint

---
+++ p2/1a/hello.c
@@ -0,0 +1,8 @@
+/* Hello World program */
+
+#include<stdio.h>
+
+void main(){
+	    printf("Hello World");
+
+}
--- p2/1a/mysh.c
+++ p2/1a/mysh.c
@@ -22,17 +22,23 @@ void usage(char *prog) {
 	exit(1);
 }
 
-void fatal_error(){
-}
-
 int main(int argc, char *argv[]) {
 	//Constants
 	const int MAX_LINE = 512;
+	int batch_mode = 0;
 	char error_message[30] = "An error has occurred\n";
-
+	FILE* target_stream;
 	//Check should I run in batch mode?
-	if(argc > 2){
+	if(argc >= 2){
 		//Do batch stuff
+		target_stream = fopen(argv[1], "r");
+		if (target_stream == NULL) {
+			write(STDERR_FILENO, error_message, strlen(error_message));
+			exit(1);
+		}
+		batch_mode = 1;
+	} else {
+		target_stream = stdin;
 	}
 
 
@@ -40,7 +46,10 @@ int main(int argc, char *argv[]) {
 	int done = 1;
 
 	while(done){
-		printf("mysh> ");
+
+		if(!batch_mode){
+			printf("mysh> ");
+		}
 
 		//Read input variables
 		int argsize;
@@ -49,11 +58,26 @@ int main(int argc, char *argv[]) {
 		int new_argc = 0;
 		int r; // Result for stuff
 		char* arg;
-		
-		if(fgets(in, MAX_LINE, stdin) == NULL){
-			fatal_error();
+
+		if(fgets(in, MAX_LINE + 1, target_stream) == NULL){
+			if(feof(target_stream)){
+				//Read till the end of file for batch mode
+				break;
+			}else{
+				write(STDERR_FILENO, error_message, strlen(error_message));
+			}
 		}
-		//printf("%d", strlen(in));
+		if(strlen(in) == 512){
+			write(STDERR_FILENO, error_message, strlen(error_message));
+			char c;
+			scanf("%c%*[^\n]%*c", &c);
+			continue;
+		}
+
+		if(batch_mode){ //Print stuff out if batch mode
+			write(STDOUT_FILENO, in, strlen(in));
+		}
+
 		if(strlen(in) == 1){
 			continue; //Just a return char
 		}
@@ -62,7 +86,6 @@ int main(int argc, char *argv[]) {
 		memset(&new_argv, 0, sizeof(new_argv));
 
 		strtok(in, "\n"); //Trick to remove new lines
-
 		new_argc = 0;
 		arg = strtok (in, " ");
 		while (arg != NULL) {
@@ -70,7 +93,7 @@ int main(int argc, char *argv[]) {
 			new_argc++;
 			arg = strtok (NULL, " ");
 		}
-
+		
 		//Checking for builtin commands
 		if(!strcmp(new_argv[0], "exit")){
 			break;	
@@ -96,7 +119,8 @@ int main(int argc, char *argv[]) {
 			//Code for pwd
 			char cwd[1024];
 			if (getcwd(cwd, sizeof(cwd)) != NULL){
-				printf("%s\n", cwd);
+				write(1, cwd, strlen(cwd));
+				write(1, "\n", 1); //Lazy hack to pass test
 			}else{
 				write(STDERR_FILENO, error_message, strlen(error_message));
 			}
@@ -109,11 +133,37 @@ int main(int argc, char *argv[]) {
   		pid_t tpid;
     	int child_status;
 
+		//Do fun feature, c compiler
+		int fun_feature = 0;
+		int arglength = strlen(new_argv[0]);
+		if(new_argv[0][arglength - 1] == 'c' && new_argv[0][arglength - 2] == '.'){
+			
+			char* gcc_args[4];
+			gcc_args[0] = "gcc";
+			gcc_args[1] = "-o";
+			gcc_args[2] = "/tmp/run";
+			gcc_args[3] = new_argv[0];
+
+			//Replace running proggy
+			new_argv[0] = "/tmp/run";
+			//Forking for gcc
+			child_pid = fork();
+			if(child_pid == 0) {
+				execvp("gcc", gcc_args);
+				write(STDERR_FILENO, error_message, strlen(error_message));
+				exit(1);
+			}else {
+				wait(&child_status);
+			}
+
+		}
+
+
 		child_pid = fork();
 		if(child_pid == 0) {
 			execvp(new_argv[0], new_argv);
-			printf("Unknown command\n");
-			exit(0);
+			write(STDERR_FILENO, error_message, strlen(error_message));
+			exit(1);
 		}else {
 			wait(&child_status);
 		}
