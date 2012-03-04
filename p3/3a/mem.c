b3f4295702f6a2cd78f1acdcfac314cc338d6b10
Terrence <vener88@gmail.com>
2012-03-04 16:15:04 -0600

    Added structure for p3 and 3a

---
+++ p3/3a/mem.c
@@ -0,0 +1,20 @@
+#include "mem.h"
+
+int Mem_Init(int sizeOfRegion, int debug){
+	
+	return 0;
+}
+
+void *Mem_Alloc(int size){
+	int x = 0;
+	int* px = &x;
+	return px;
+}
+
+int Mem_Free(void *ptr, int coalesce){
+	return 0;
+}
+
+void Mem_Dump(){
+
+}
---
+++ p3/3a/memtest.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "mem.h"
+
+int main(int argc, const char *argv[]){
+	printf("Testing out malloc...\n");
+	return 0;
+}
