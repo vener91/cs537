7571778c338ce333eb3c0218ec8b17ecdcb485c1
Terrence <vener88@gmail.com>
2012-04-14 18:46:20 -0500

    Added syscalls

--- p4/4b/user/user.h
+++ p4/4b/user/user.h
@@ -25,6 +25,8 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int clone(void(*fcn)(void*), void *arg, void*stack);
+int join(void **stack);
 
 // user library functions (ulib.c)
 int stat(char*, struct stat*);
